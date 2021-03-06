2d9f6780f913fd5639e07a4b025ff07e71546a67
inatoff <inatoff@gmail.com>
2021-03-06 20:00:09 +0200

    Add create-react-app with axios, typescript and allowed CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,13 @@ namespace API
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
       });
       services.AddDbContext<DataContext>(opt => opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+      services.AddCors(opt => 
+      {
+        opt.AddPolicy("CorsPolicy", policy => 
+        {
+          policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +52,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors("CorsPolicy");
 
       app.UseAuthorization();
 
